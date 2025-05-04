2a4d166b95f1018657a834c6e12ddc37359195f3
Arddian Darmawan_2200018157 <128204076+Arddiand761@users.noreply.github.com>
2025-05-04 13:34:25 +0700

    AOS

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,8 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+AOS.init()
 createApp(App).mount("#app");
