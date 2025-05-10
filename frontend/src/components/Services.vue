23a7ae32ea0f46e78275d8027e81f7bf2ea968a5
fdhliakbar <fadhliakbar125@gmail.com>
2025-05-10 13:12:28 +0700

    fix: comment out OurTeam component in App.vue and update Services.vue text to 'Legal Services'

--- frontend/src/components/Services.vue
+++ frontend/src/components/Services.vue
@@ -7,7 +7,7 @@
         <p
           class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-sky-500 text-white"
         >
-          Brand new
+          Services
         </p>
       </div>
       <h2
@@ -39,7 +39,7 @@
 
           <span class="relative">The</span>
         </span>
-        quick, brown fox jumps over a lazy dog
+        Legal Services
       </h2>
       <p class="text-base text-gray-700 md:text-lg">
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem
