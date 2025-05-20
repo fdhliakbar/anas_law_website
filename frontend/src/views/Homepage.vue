b795a53054698206c1a16338ab6fe77a001dcb3a
Arddian Darmawan_2200018157 <128204076+Arddiand761@users.noreply.github.com>
2025-05-20 16:26:18 +0700

    MEssage

--- frontend/src/views/Homepage.vue
+++ frontend/src/views/Homepage.vue
@@ -1,26 +1,21 @@
+<script setup>
+import Header from '../components/Header.vue'
+import HeroSection from '../components/HeroSection.vue'
+import About from '../components/About.vue'
+import Services from '../components/Services.vue'
+import OurTeam from '../components/OurTeam.vue'
+import Testimonials from '../components/Testimonials.vue'
+import Footer from '../components/Footer.vue'
+</script>
+
 <template>
   <Header />
   <HeroSection />
-  <ImageSlider />
   <Services />
+  <!-- <OurTeam /> -->
   <About />
   <Testimonials />
-  <button @click="goToLogin">Go to Login</button>
   <Footer />
 </template>
 
-<script setup>
-import { useRouter } from "vue-router";
-import Header from "../components/Header.vue";
-import HeroSection from "../components/HeroSection.vue";
-import ImageSlider from "../components/ImageSlider.vue";
-import About from "../components/About.vue";
-import Services from "../components/Services.vue";
-import Testimonials from "../components/Testimonials.vue";
-import Footer from "../components/Footer.vue";
-
-const router = useRouter();
-const goToLogin = () => {
-  router.push("/login");
-};
-</script>
+<style scoped></style>
