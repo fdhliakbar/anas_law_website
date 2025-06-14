1001b0ac35910b44a535d1bdfe7c5f5bc7469f5f
Arddd <128204076+Arddiand761@users.noreply.github.com>
2025-06-14 15:21:33 +0700

    Arddian be (#28)
    * jangan di merge dil
    
    * Fungsional users

--- backend/nuxt.config.ts
+++ backend/nuxt.config.ts
@@ -1,14 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// file: nuxt.config.ts
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  
+
   runtimeConfig: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    STACK_SECRET_SERVER_KEY: process.env.STACK_SECRET_SERVER_KEY,
+    // Definisikan semua variabel database di sini
+    //
+    //
+    databaseUrl: process.env.DATABASE_URL,
+    // Anda bisa tetap menyimpan variabel lain di sini
+    stackSecretServerKey: process.env.STACK_SECRET_SERVER_KEY,
+
     public: {
-      STACK_PROJECT_ID: process.env.NUXT_PUBLIC_STACK_PROJECT_ID,
-      STACK_PUBLISHABLE_CLIENT_KEY: process.env.NUXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY
-    }
-  }
-})
+      stackProjectId: process.env.NUXT_PUBLIC_STACK_PROJECT_ID,
+      stackPublishableClientKey:
+        process.env.NUXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY,
+    },
+  },
+});
